faf1f34c82f280f29a8a2b44c30a9090f3cfb421
Isshu Rakusai <rakusai@kamilabo.jp>
2017-02-08 02:26:03 +0900

    change order param

--- lib/datastoreid/criteria/sortable.rb
+++ lib/datastoreid/criteria/sortable.rb
@@ -2,14 +2,23 @@ module Datastoreid
   class Criteria
     # Defines behaviour for sort operations.
     module Sortable
-      # order('user_id', :asc)
-      # order('user_id', :desc)
-      def order(*args)
-        set_query(query.order(*args))
+      # order(created_at: :asc)
+      # order(created_at: :desc)
+      # order('created_at')
+      # order(:created_at)
+      def order(criterion)
+        if criterion.is_a? ::Hash
+          criterion.each_pair do |key, value|
+            set_query(query.order(key.to_s, value))
+          end
+        elsif criterion.is_a? ::String
+          set_query(query.order(criterion.to_s))
+        end
         self
       end
 
       alias_method :sort, :order
+      alias_method :order_by, :order
     end
   end
 end

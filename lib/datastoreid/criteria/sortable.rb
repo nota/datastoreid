843890ec6c7599807f187b8cf348cedbde236adb
Isshu Rakusai <rakusai@kamilabo.jp>
2017-02-08 04:54:07 +0900

    fix bug of alias property name for order

--- lib/datastoreid/criteria/sortable.rb
+++ lib/datastoreid/criteria/sortable.rb
@@ -9,10 +9,13 @@ module Datastoreid
       def order(criterion)
         if criterion.is_a? ::Hash
           criterion.each_pair do |key, value|
+            key = parent_entry.original_properties_names[key] || key
             set_query(query.order(key.to_s, value))
           end
         elsif criterion.is_a? ::String
-          set_query(query.order(criterion.to_s))
+          key = criterion
+          key = parent_entry.original_properties_names[key.to_s] || key
+          set_query(query.order(key.to_s))
         end
         self
       end

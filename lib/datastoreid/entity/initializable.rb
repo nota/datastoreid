4869a99182e6c04d71da15c57b5c908ceaaad046
Isshu Rakusai <rakusai@kamilabo.jp>
2017-02-10 21:07:56 +0900

    allow to use string for key(id)

--- lib/datastoreid/entity/initializable.rb
+++ lib/datastoreid/entity/initializable.rb
@@ -9,7 +9,7 @@ module Datastoreid
           set_default_values
           if options.is_a? Google::Cloud::Datastore::Entity
             properties_names.each { |a| send("#{a}=", options[a.to_s]) }
-            send('id=', options.key.id)
+            send('id=', options.key.id || options.key.name)
           elsif options.is_a? ::Hash
             options.each_pair { |key, value| send("#{key}=", value) }
           end
